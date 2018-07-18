ac816549a26f0d0e6eba090f61e97f0906c42d75
Matt Shull <derekshull@gmail.com>
2018-07-17 23:26:47 -0500

    update gitignore

--- handler.js
+++ handler.js
@@ -1,18 +1,47 @@
 'use strict';
+var azure = require('azure-storage');
 
-/* eslint-disable no-param-reassign */
+module.exports = {
+  store: function (context, req) {
+    context.log('request body: ', req.body);
 
-module.exports.store = function (context, req) {
-  context.log('request body: ', req.body);
+    const successRes = {
+      headers:{
+        'content-type':'application/json',
+      },
+      body: {
+        'message':'success',
+      }
+    };
 
-  const res = {
-    headers:{
-      'content-type':'application/json',
-    },
-    body: {
-      'message':'success',
-    }
-  };
+    const failRes = {
+      status: 500,
+      headers:{
+        'content-type':'application/json',
+      },
+      body: {
+        'message':'failed',
+      }
+    };
 
-  context.done(null, res);
+    var retryOperations = new azure.ExponentialRetryPolicyFilter();
+    var queueSvc = azure.createQueueService().withFilter(retryOperations);
+    queueSvc.createQueueIfNotExists('beaconprocessingqueue', function(createQueueError, createQueueResults, createQueueResponse){
+      if(!createQueueError) {
+        queueSvc.createMessage('beaconprocessingqueue', req.body, function(createMessageError, createMessageResults, createMessageResponse){
+          if(!createMessageError) {
+            context.done(null, successRes);
+          } else {
+            context.done(null, failRes);
+          }
+        });
+      } else {
+        context.done(null, failRes);
+      }
+    });
+  },
+  process: function(context, item) {
+    context.log(`Received item: ${item}`);
+    context.done();
+  }
 };
